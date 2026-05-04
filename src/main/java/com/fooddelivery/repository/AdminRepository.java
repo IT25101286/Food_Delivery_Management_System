42b99ee41e2b61169fc9106273f9d85b1665036d
Ranudi Thilakarathna <it25300026@my.sliit.lk>
2026-05-04 20:14:21 +0530

    second commit

---
+++ src/main/java/com/fooddelivery/repository/AdminRepository.java
@@ -0,0 +1,12 @@
+package com.fooddelivery.repository;
+
+import com.fooddelivery.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findByEmail(String email);
+}
